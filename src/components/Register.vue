50c2e98c70fe21e29ab24e4a65421db7a35b5ea4
hardik25252075 <hardik.jain@globallogic.com>
2022-10-31 16:19:41 +0530

    email-validation

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,12 +1,7 @@
 <template>
   <v-container class="container" fill-height>
     <v-row class="text-center" justify="center">
-      <v-col
-        cols="12"
-        sm="5"
-        md="5"
-        lg="3"
-      >
+      <v-col cols="12" sm="5" md="5" lg="3">
         <v-card elevation="3" outlined style="padding: 20px">
           <v-img
             :src="require('../assets/logo.svg')"
@@ -22,11 +17,11 @@
               <validation-provider
                 v-slot="{ errors }"
                 name="Name"
-                rules="required|max:10"
+                rules="required|max:15"
               >
                 <v-text-field
                   v-model="name"
-                  :counter="10"
+                  :counter="15"
                   :error-messages="errors"
                   label="Name"
                   required
@@ -49,13 +44,27 @@
               <validation-provider
                 v-slot="{ errors }"
                 name="Password"
-                rules="required|max:12"
+                rules="required|min:4|max:12"
               >
                 <v-text-field
                   v-model="password"
                   :counter="12"
                   :error-messages="errors"
                   label="Password"
+                  required
+                ></v-text-field>
+              </validation-provider>
+              <!-- Confirm Password -->
+              <validation-provider
+                v-slot="{ errors }"
+                name="Confirm Password"
+                rules="required|min:4|max:12"
+              >
+                <v-text-field
+                  v-model="confirmPassword"
+                  :counter="12"
+                  :error-messages="errors"
+                  label="Confirm Password"
                   required
                 ></v-text-field>
               </validation-provider>
@@ -70,16 +79,12 @@
                 class="mb-5"
               ></v-checkbox>
 
-              <v-btn
-                color="primary"
-                depressed
-                elevation="2"
-                type="submit"
-              >
+              <v-btn color="primary" depressed elevation="2" type="submit">
                 Register
               </v-btn>
             </form>
           </validation-observer>
+
           <v-main style="padding-top: 15px">
             <router-link to="/login"> Return to login </router-link>
           </v-main>
@@ -91,7 +96,14 @@
 
 
 <script>
-import { required, digits, email, max, regex } from "vee-validate/dist/rules";
+import {
+  required,
+  digits,
+  email,
+  min,
+  max,
+  regex,
+} from "vee-validate/dist/rules";
 import {
   extend,
   ValidationObserver,
@@ -108,22 +120,27 @@ setInteractionMode("eager");
 
 extend("password", {
   ...digits,
-  message: "{field} needs to be {length} digits. ({value})",
+  message: "{_field_} needs to be {length} digits. ({_value_})",
 });
 
 extend("required", {
   ...required,
-  message: "{field} can not be empty",
+  message: "{_field_} can not be empty",
 });
 
 extend("max", {
   ...max,
-  message: "{field} may not be greater than {length} characters",
+  message: "{_field_} may not be greater than {length} characters",
+});
+
+extend("min", {
+  ...min,
+  message: "{_field_} should be greater than {length} characters",
 });
 
 extend("regex", {
   ...regex,
-  message: "{field} {value} does not match {regex}",
+  message: "{_field_} {_value_} does not match {regex}",
 });
 
 extend("email", {
@@ -132,15 +149,16 @@ extend("email", {
 });
 
 export default {
-    name:"RigisterPage",
   components: {
     ValidationProvider,
     ValidationObserver,
   },
+  name: "RegisterPage",
   data: () => ({
     name: "",
     email: "",
     password: "",
+    confirmPassword: "",
     checkbox: true,
   }),
 
